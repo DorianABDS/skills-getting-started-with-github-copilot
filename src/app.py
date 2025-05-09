703637920aca14df885ba603a557abf599c3c944
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-05-09 01:30:17 +0000

    add space

--- src/app.py
+++ src/app.py
@@ -74,7 +74,7 @@ activities = {
         "schedule": "Fridays, 4:00 PM - 5:00 PM",
         "max_participants": 12,
         "participants": ["ella@mergington.edu", "logan@mergington.edu"]
-    }  
+    }   
 }
 
 
